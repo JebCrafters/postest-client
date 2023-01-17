144699c08555f5964c884e3f6a2df3e5696e79e5
JebCrafters <JebCrafters@gmail.com>
2023-01-17 11:40:36 +0100

    remove some designe

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,25 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import './profile.css'
-import './shakal.css'
+import "./profile.css";
 
-
-const Profile = () => {
-  const currentUser = useSelector((state) => state.user.currentUser.email);
-  const str_user = String(currentUser);
-  var background = ""
-  if (str_user == "shakal@gmail.com") {
-    background = "shakal";
-  } else {
-    background = "profile_background";
-  }
-  return (
-    <>
-      <div className={`${background}`}>
-        <h1 className="text">Welcome to you profile</h1>
-      </div>
-    </>
-  );
-};
+const Profile = () => {};
+return (
+  <>
+    <div className="profile_background">
+      <h1 className="text">Welcome to you profile</h1>
+    </div>
+  </>
+);
 
 export default Profile;
