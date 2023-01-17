00714d3ba2bece42234c30608a4c68c1ad1217ce
JebCrafters <JebCrafters@gmail.com>
2023-01-17 14:06:23 +0100

    resize posts

--- src/components/posts/post.jsx
+++ src/components/posts/post.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import "../posts/formPost.css"
+import "../posts/formPost.css";
 
-
-const Post =  (props)  => {
-  
+const Post = (props) => {
   let resizeTimer;
-window.addEventListener("resize", () => {
-  document.body.querySelector("form__Post").add("resize-animation-stopper");
-  clearTimeout(resizeTimer);
-  resizeTimer = setTimeout(() => {
-    document.body.querySelector("form__Post").remove("resize-animation-stopper");
-  }, 100);
-});
+  window.addEventListener("resize", () => {
+    var element = document.getElementById("form__Post");
+    element.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      var element = document.getElementById("form__Post");
+      element.classList.remove("resize-animation-stopper");
+    }, 100);
+  });
 
   return (
     <>
-      <div className="form__Post" style= {{backgroundColor: props.post.color}} >
+      <div id="form__Post" style={{ backgroundColor: props.post.color }}>
         <h1 className="post_name"> {props.post.namePost} </h1>
         <div className="form__inputTextArea">
-          <h1 className="post_text"  > {props.post.textPost} </h1>
+          <h1 className="post_text"> {props.post.textPost} </h1>
         </div>
         <div className="input__dateAuthor">
           <h1 className="date"> {props.post.dateTime} </h1>
