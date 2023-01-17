fc673e85c8aefe53a761c8a5e0641714f475fc29
JebCrafters <JebCrafters@gmail.com>
2023-01-17 11:43:25 +0100

    remove some designe

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./profile.css";
 
-const Profile = () => {};
-return (
-  <>
-    <div className="profile_background">
-      <h1 className="text">Welcome to you profile</h1>
-    </div>
-  </>
-);
-
+const Profile = () => {
+  return (
+    <>
+      <div className="profile_background">
+        <h1 className="text">Welcome to you profile</h1>
+      </div>
+    </>
+  );
+};
 export default Profile;
